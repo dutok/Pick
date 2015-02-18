6905b422004619ab1170d40c012b0e0933382456
Dutok <droiduser560@gmail.com>
2015-02-18 09:38:28 +0000

    Optimized panel web page

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/codegangsta/negroni"
-	oauth2 "github.com/goincremental/negroni-oauth2"
-	sessions "github.com/goincremental/negroni-sessions"
-	"github.com/goincremental/negroni-sessions/cookiestore"
-	"github.com/gorilla/mux"
+    oauth2 "github.com/goincremental/negroni-oauth2"
+    sessions "github.com/goincremental/negroni-sessions"
+    "github.com/goincremental/negroni-sessions/cookiestore"
+    "github.com/gorilla/mux"
+    "github.com/phyber/negroni-gzip/gzip"
 )
 
 var err error
@@ -19,29 +20,30 @@ func main() {
 
 func httpServer(server *Server) {
 	secureMux := mux.NewRouter()
-
+	
 	loadRoutes(secureMux, server)
 
-	secure := negroni.New()
-	secure.Use(oauth2.LoginRequired())
-	secure.UseHandler(secureMux)
+    secure := negroni.New()
+    //secure.Use(oauth2.LoginRequired())
+    secure.UseHandler(secureMux)
 
-	n := negroni.New()
-	n.Use(sessions.Sessions("my_session", cookiestore.New([]byte("secret123"))))
-	n.Use(oauth2.Google(&oauth2.Config{
-		ClientID:     "824000373870-148afj3scuj2fururtrn2ffn9vu48rfs.apps.googleusercontent.com",
-		ClientSecret: "tB9cqq53V1H0yXjsp1SGKcDv",
-		RedirectURL:  "http://dutok.koding.io/oauth2callback",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-	}))
+    n := negroni.New()
+    n.Use(sessions.Sessions("my_session", cookiestore.New([]byte("secret123"))))
+    n.Use(oauth2.Google(&oauth2.Config{
+        ClientID:     "824000373870-148afj3scuj2fururtrn2ffn9vu48rfs.apps.googleusercontent.com",
+        ClientSecret: "tB9cqq53V1H0yXjsp1SGKcDv",
+        RedirectURL:  "http://dutok.koding.io/oauth2callback",
+        Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+    }))
+    n.Use(gzip.Gzip(gzip.BestSpeed))
 
-	router := mux.NewRouter()
+    router := mux.NewRouter()
 
-	//There is probably a nicer way to handle this than repeat the restricted routes again
-	//of course, you could use something like gorilla/mux and define prefix / regex etc.
-	router.PathPrefix("/").Handler(secure)
+    //There is probably a nicer way to handle this than repeat the restricted routes again
+    //of course, you could use something like gorilla/mux and define prefix / regex etc.
+    router.PathPrefix("/").Handler(secure)
 
-	n.UseHandler(router)
+    n.UseHandler(router)
 
-	n.Run(":80")
+    n.Run(":80")
 }
